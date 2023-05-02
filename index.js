3ddd1cde36bd694f9b79e0f84b63210e969bbeff
slystersanto <santhosh.mech.19@gmail.com>
2023-05-02 17:14:36 +0530

    last

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.use((req, res, next) => {
+   
+    res.setHeader('Access-Control-Allow-Origin', 'https://chic-rolypoly-fc9eb3.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 const DB = process.env.DB;
 const client = new MongoClient(DB, { useNewUrlParser: true, useUnifiedTopology: true });
 
